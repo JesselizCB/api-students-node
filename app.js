93b7ed0e2a79e9e71f9e02a1e7a9bca87d046549
Yesseliz Choque Becerra <48224215+JesselizCB@users.noreply.github.com>
2025-09-07 23:05:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.delete('/student/:id', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8005;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 }); 
